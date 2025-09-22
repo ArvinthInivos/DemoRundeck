c5d475157500474503f68ee2bd192ad435a8e0d2
Arvinth-Ruckshan <arvinth.dillruckshan@inivosglobal.com>
2025-09-22 12:38:35 +0530

    added

--- hello-world-api/Startup.cs
+++ hello-world-api/Startup.cs
@@ -27,14 +27,19 @@ namespace hello_world_api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
